7ba51b0e7e74982ae6f58c8bdf84966185808962
vitorcapdeville <vgcapdeville@hotmail.com>
2024-06-16 19:11:03 -0300

    ♻️ Refatorar proxy.

--- app/utils.py
+++ app/utils.py
@@ -1,20 +1,17 @@
 import httpx
 from fastapi import HTTPException, Request
 
-
 async def proxy(request: Request, path: str, service_url: str):
     url = f"{service_url}/{path}"
     async with httpx.AsyncClient() as client:
-        if request.method == "GET":
-            response = await client.get(url, params=request.query_params)
-        elif request.method == "POST":
+        headers = dict(request.headers)
+        data = None
+        if request.method in ["POST", "PUT"]:
             data = await request.body()
-            headers = dict(request.headers)
-            response = await client.post(url, data=data, headers=headers, params=request.query_params)
-        elif request.method == "PUT":
-            data = await request.body()
-            headers = dict(request.headers)
-            response = await client.put(url, data=data, headers=headers, params=request.query_params)
+        try:
+            response = await client.request(request.method, url, data=data, headers=headers, params=request.query_params)
+        except httpx.HTTPStatusError as exc:
+            raise HTTPException(status_code=exc.response.status_code, detail=str(exc))
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=response.text)
     return response.json()
